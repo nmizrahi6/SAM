faa2009d3af382ad1bdca2de4fee61bf725e638a
Natanel Mizrahi <natanel.mizrahi@gmail.com>
2022-09-19 22:41:10 +0300

    add virus total + .env

--- app.py
+++ app.py
@@ -1,7 +1,49 @@
-from flask import Flask
+import vt
+import re
+import os
+from flask import Flask, request
+from dotenv import load_dotenv
+
+ip_pattern = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
+url_pattern = re.compile("^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$")
+
+load_dotenv()
+VIRUSTOTAL_API_KEY = os.getenv('VIRUSTOTAL_API_KEY')
+DEBUG_ENABLED = bool(os.getenv('DEBUG'))
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.route("/", methods=['GET'])
+def analyze_query():
+    try:
+        client = vt.Client(VIRUSTOTAL_API_KEY)
+        address = search_ip_or_url(request.args.get("question"))
+        url_id = vt.url_id(address)
+        url = client.get_object("/urls/{}", url_id)
+        stats = url.last_analysis_stats
+        is_malicious = stats["malicious"] > 0
+        return f"""
+        [{address=}][{is_malicious=}]
+        <br>
+        score: {stats["malicious"]}/{stats["harmless"] + stats["malicious"]}
+        <br>
+        {stats["malicious"] or "Zero"} security vendors flagged {address} as malicious
+        """
+    except vt.ClientConnectorError as e:
+        return f'Failed to connect to VirusTotal:{e}'
+    except Exception as e:
+        return "Are you trying to get me crazy???  :("
+
+
+def search_ip_or_url(question):
+    return search_pattern(question, ip_pattern) or search_pattern(question, url_pattern)
+
+
+def search_pattern(question, pattern):
+    search = pattern.search(question)
+    return search.group() if search else None
+
+
+if __name__ == "__main__":
+    app.run(debug=DEBUG_ENABLED)

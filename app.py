2869e17b9c75a3abd004b959c9ea302fda2c9780
t-etsemachmicrosoft.com <t-etsemach@microsoft.com>
2022-09-20 10:58:18 +0300

    added Chatbot

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import random
+
 import vt
 import re
 import os
@@ -5,13 +7,17 @@ from flask import Flask, request
 from dotenv import load_dotenv
 
 ip_pattern = re.compile(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
-url_pattern = re.compile("^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$")
+url_pattern = re.compile(
+    "^https?:\\/\\/(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$")
 
 load_dotenv()
 VIRUSTOTAL_API_KEY = os.getenv('VIRUSTOTAL_API_KEY')
 DEBUG_ENABLED = bool(os.getenv('DEBUG'))
 
 app = Flask(__name__)
+
+fileName = "BotWords.txt"
+splitterSymbol = '|'
 
 
 @app.route("/", methods=['GET'])
@@ -34,6 +40,25 @@ def analyze_query():
         return f'Failed to connect to VirusTotal:{e}'
     except Exception as e:
         return "Are you trying to get me crazy???  :("
+
+
+@app.route('/bot', methods=['GET'])
+def sendToBot():
+    text = request.args.get("question").lower()
+    with open(fileName) as f:
+        lines = f.readlines()
+        Words = {}
+
+        for i in range(0, len(lines) - 1, 2):
+            Words[(lines[i][lines[i].rfind(":") + 1:lines[i].rfind("\\")]).lower()] = \
+                (lines[i + 1][lines[i + 1].rfind(":") + 1:lines[i + 1].rfind("\\")]).lower().split(splitterSymbol)
+
+        if Words.get(text) == None:
+            return "Unknown text"
+
+        randomIndex = random.randint(0, len(Words.get(text)) - 1)
+
+        return Words.get(text)[randomIndex]
 
 
 def search_ip_or_url(question):
